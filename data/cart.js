4e3c54ef22e454a21c6fdf3370405ad06c237c26
Prashant <devmail.prashant@gmail.com>
2026-01-03 20:28:11 +0530

    cart by js

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [
+  {
+    ID: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quant: 1,
+  },
+  {
+    ID: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quant: 2,
+  },
+];
 export function addtocart(productID) {
   let matchingItem = true;
   cart.forEach((item) => {
