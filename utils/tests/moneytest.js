615cc1cb55eb0a3df4706ba73303cea6236fa82a
rootX <devmail.prashant@gmail.com>
2026-02-19 22:33:44 +0530

    till testing mid

---
+++ utils/tests/moneytest.js
@@ -0,0 +1,10 @@
+import { formatCurrency } from "../money.js";
+
+if (formatCurrency(2095) === "20.95") console.log("passed");
+else console.log("failed");
+
+if (formatCurrency(0) === "0.00") console.log("passed");
+else console.log("failed");
+
+if (formatCurrency(2000.5) === "20.00") console.log("passed");
+else console.log("failed");
